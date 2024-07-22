864eb9e6c965614b929ccd48c8d2fe82563bd849
Ben <thescientist771.mail@gmail.com>
2024-07-22 19:56:43 +0100

    v0.0.3
    Working Towards #8, added a navigation bar, happy with desktop appearance and animations, mobile tests to come. Also added the "title" section, need to decide if I'm going to use the progress tags or create a custom progress bar for cross-browser styling purposes

---
+++ onboarding/courses.php
@@ -0,0 +1,61 @@
+<!--
+ ____  ____  _  _    _    _    __    __    __      __    ___  ____
+(  _ \( ___)( \( )  ( \/\/ )  /__\  (  )  (  )    /__\  / __)( ___)
+ ) _ < )__)  )  (    )    (  /(__)\  )(__  )(__  /(__)\( (__  )__)
+(____/(____)(_)\_)  (__/\__)(__)(__)(____)(____)(__)(__)\___)(____)
+
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <!-- Tab Styling -->
+    <title>Chronos</title>
+    <link rel="icon" href="../img/chronos.png">
+
+    <!-- Responsive Design Tag -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Other Meta Tags -->
+    <meta property="og:title" content="Chronos">
+    <meta property="og:description" content="Manage your homework and timetable better with Chronos.">
+    <meta property="og:url" content="http://chronos.theiceburg.net/onboarding/courses">
+
+    <!-- Font Awesome Import -->
+    <script src="https://kit.fontawesome.com/7daaf9098f.js" crossorigin="anonymous"></script>
+
+    <!-- Font Import -->
+
+
+    <!-- Page Styling -->
+    <link rel="stylesheet" href="../stylesheets/onboarding/courses.css">
+    <link rel="stylesheet" href="../stylesheets/style.css">
+    <link rel="stylesheet" href="../stylesheets/nav-bar.css">
+
+</head>
+<body>
+    <nav id="nav-bar">
+        <ul id="nav-list">
+            <li><a class="nav-left nav-element" href="../"><img src="../img/chronos.png" alt="Chronos Logo" id="nav-logo"></a></li>
+            <li><a id="nav-title" class="nav-left nav-element" href="../"><b>Chronos</b></a></li>
+
+            <li><a id="nav-settings" class="nav-right nav-element" href="../settings"><i class="fa-solid fa-gear"></i></a></li>
+            <li><a id="nav-launchpad" class="nav-right nav-element" href="../launchpad"><i class="fa-solid fa-rocket"></i> <span id="nav-launchpad-text">Launchpad</span></a></li>
+            <li><a id="nav-timetable" class="nav-right nav-element" href="../timetable"><i class="fa-solid fa-calendar-days"></i> Timetable</a></li>
+            <li><a id="nav-tasks" class="nav-right nav-element" href="../tasks"><i class="fa-solid fa-circle-check"></i> Tasks</a></li>
+        </ul>
+    </nav>
+    <div id="page-content">
+        <div id="page-title-container">
+            <h1 id="page-title">1 / 3 • Course Creation</h1>
+            <progress id="onboarding-progress" value="33" max="100"></progress>
+            <p id="onboarding-progress-desc">Onbarding Progress (33% Complete)</p>
+        </div>
+        <div id="course-tiles">
+        </div>
+    </div>
+    <footer id="footer">
+
+    </footer>
+</body>
