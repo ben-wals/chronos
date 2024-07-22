3593ea2787c37454b84a4d94720ce67b84ec3452
Ben <thescientist771.mail@gmail.com>
2024-07-22 21:13:19 +0100

    v0.0.5
    added some comments, a font and a footer

--- onboarding/courses.php
+++ onboarding/courses.php
@@ -26,36 +26,58 @@
     <script src="https://kit.fontawesome.com/7daaf9098f.js" crossorigin="anonymous"></script>
 
     <!-- Font Import -->
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
     <!-- Page Styling -->
     <link rel="stylesheet" href="../stylesheets/onboarding/courses.css">
     <link rel="stylesheet" href="../stylesheets/style.css">
     <link rel="stylesheet" href="../stylesheets/nav-bar.css">
+    <link rel="stylesheet" href="../stylesheets/footer.css">
 
 </head>
 <body>
+
+    <!-- Navigation Bar -->
     <nav id="nav-bar">
         <ul id="nav-list">
+
+            <!-- Left Side Elements -->
             <li><a class="nav-left nav-element" href="../"><img src="../img/chronos.png" alt="Chronos Logo" id="nav-logo"></a></li>
             <li><a id="nav-title" class="nav-left nav-element" href="../"><b>Chronos</b></a></li>
 
+            <!-- Right Side Elements -->
             <li><a id="nav-settings" class="nav-right nav-element" href="../settings"><i class="fa-solid fa-gear"></i></a></li>
             <li><a id="nav-launchpad" class="nav-right nav-element" href="../launchpad"><i class="fa-solid fa-rocket"></i> <span id="nav-launchpad-text">Launchpad</span></a></li>
             <li><a id="nav-timetable" class="nav-right nav-element" href="../timetable"><i class="fa-solid fa-calendar-days"></i> Timetable</a></li>
             <li><a id="nav-tasks" class="nav-right nav-element" href="../tasks"><i class="fa-solid fa-circle-check"></i> Tasks</a></li>
+
         </ul>
     </nav>
+
+    <!-- Page Content -->
     <div id="page-content">
+
+        <!-- Page Titles -->
         <div id="page-title-container">
             <h1 id="page-title">1 / 3 • Course Creation</h1>
             <progress id="onboarding-progress" value="33" max="100"></progress>
             <p id="onboarding-progress-desc">Onbarding Progress (33% Complete)</p>
         </div>
+
         <div id="course-tiles">
         </div>
-    </div>
-    <footer id="footer">
 
+    </div>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <ul id="footer-list">
+            <li><a class="nav-left nav-element" href="../"><img src="../img/chronos.png" alt="Chronos Logo" id="nav-logo"></a></li>
+            <li><a id="nav-title" class="nav-left nav-element" href="../"><b>Chronos</b></a></li>
+            <li><span id="footer-legal" class="footer-right">Powered By <a class="footer-link" href="https://theiceburg.net">The Iceburg</a><br>© Ben Wallace <?php echo date("Y"); ?></span></li>
+            <li><span id="footer-report" class="footer-right">Found and issue with this page? Report it <a class="footer-link" href="https://github.com/ben-wals/chronos/issues/new?title=Ref%3A%20onboarding%2Fcourses">here</a>.</span></li>
+        </ul>
     </footer>
 </body>
