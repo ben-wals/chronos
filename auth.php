6837e929509e34015964eb7eb199bea27c4dcb42
Ben Wallace <thescientist771.mail@gmail.com>
2024-10-20 18:29:25 +0100

    v0.1.3
    Login now works, not fully functional still requiring the addition of failsafes and input validations

--- auth.php
+++ auth.php
@@ -1,5 +1,19 @@
-<?php 
+<?php
+
+    session_start();
+
     include './classes/db.php';
 
-    $acc = new acc(dbName: 'chronos_acc');
+    $acc = new acc($_POST['username'], dbName: 'chronos_acc');
+
+    session_regenerate_id();
+    $_SESSION['loggedin'] = TRUE;
+    $_SESSION['name'] = $_POST['username'];
+    $_SESSION['id'] = $id;
+    
+    if (password_verify($_POST['password'], $acc->getAccountInfo('Password'))) {
+        header("Location: ./launchpad");
+    }
+    
+
 ?>
--- classes/db.php
+++ classes/db.php
@@ -1,7 +1,7 @@
 <?php
 class db {
     
-    private $con;
+    protected $con;
 
     public function __construct($dbHost = 'localhost', $dbUser = 'root', $dbPass = '', $dbName = '') {
         $this->con = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
@@ -9,16 +9,28 @@ class db {
 	        exit('Failed to connect to MySQL: ' . mysqli_connect_error());
         }
     }
+
 }
 
 class acc extends db {
 
-    public function get($tableName, $dataKey) {
-        $stmt = $this->$con->prepare('SELECT ' . $dataKey . ' FROM ' . $tableName . ' WHERE username = ?');
-        $stmt->bind_param('s', );
+    public $username;
+
+    public function __construct($username, $dbHost = 'localhost', $dbUser = 'root', $dbPass = '', $dbName = '') {
+        parent::__construct($dbHost, $dbUser, $dbPass, $dbName);
+        $this->username = $username;
+    }
+
+    public function getAccountInfo($dataKey) {
+        $stmt = $this->con->prepare('SELECT ' . $dataKey . ' FROM accounts WHERE username = ?');
+        $stmt->bind_param('s', $this->username);
         $stmt->execute();
 	    $stmt->store_result();
+        $stmt->bind_result($result);
+        $stmt->fetch();
+        return $result;
     }
     
 }
+
 ?>
